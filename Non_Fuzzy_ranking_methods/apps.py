63edb7f2507ca640dfe8e503f85ef4edafc1b2a1
M. Askari <145195116+fatemeh-askari@users.noreply.github.com>
2024-04-29 23:20:10 +0430

    basic config

---
+++ Non_Fuzzy_ranking_methods/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NonFuzzyRankingMothodsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Non_Fuzzy_ranking_methods'
